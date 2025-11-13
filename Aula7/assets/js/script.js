9a513c4a7850d2f3fd0c4d166d7bad499fb9e36a
Anderson Felipe Dias Domingos <139730074+Felipe-dmgs@users.noreply.github.com>
2025-11-13 15:35:32 -0300

    Atualizar o script.js

--- Aula7/assets/js/script.js
+++ Aula7/assets/js/script.js
@@ -4,25 +4,19 @@ const logElement = document.querySelector(".log");
 const log = new Log(logElement);
 
 function startGame(SelectedHeroClass) {
-    // 1. Esconde a seleção e mostra a arena
     selectionScreen.style.display = "none";
     fightArena.style.display = "flex"; 
     logElement.style.display = "block"; 
-
-    // 2. Cria o Herói
     let charName = prompt(`Qual o nome do seu ${SelectedHeroClass.name}?`, "Herói");
     if (!charName) { charName = SelectedHeroClass.name; }
     
     let char = new SelectedHeroClass(charName);
 
-    // 3. Cria a LISTA de monstros
     let monsterList = [
         new LittleMonster(),
         new BigMonster(),
         new Boss()
     ];
-
-    // 4. Cria o Stage (passando a lista inteira)
     const stage = new Stage(
         char,
         monsterList, // Passa o array de monstros
@@ -30,8 +24,6 @@ function startGame(SelectedHeroClass) {
         document.querySelector("#monster"),
         log
     );
-
-    // 7. E inicia o jogo (só é chamado UMA VEZ)
     stage.start();
 }
 fightArena.style.display = "none";
